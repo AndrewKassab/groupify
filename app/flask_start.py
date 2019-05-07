5cd7f3cd0c38ea0e6846a0b65cae45996b169333
Winston Durand <me@winstondurand.com>
2019-05-07 14:53:38 -0700

    (actually have falsk working... oops)

--- app/flask_start.py
+++ app/flask_start.py
@@ -4,6 +4,10 @@ import datetime
 from flask import Flask
 app = Flask(__name__)
 
+# See:
+# http://flask.pocoo.org/docs/1.0/quickstart
+# for more info on using Flask
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
