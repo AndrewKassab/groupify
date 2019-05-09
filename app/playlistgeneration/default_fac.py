c443bdcafaede732ff59a1d1c2f27f26c701a284
andrewkassab <andrewkassab@live.com>
2019-05-09 16:13:15 -0700

    refactored playlist generation code

--- app/playlistgeneration/default_fac.py
+++ app/playlistgeneration/default_fac.py
@@ -6,33 +6,26 @@ from PlaylistFactory import PlaylistFactory
 
 class DefaultPlaylistFactory(PlaylistFactory):
 
-    def __init__(self):
-        return None
-
-    # Run from constructor add users
-    def create(self):
+    # Lvl 1 filter
+    def filter_intersect(self):
         return None
 
     # Lvl 1 filter
-    def filterIntersect(self):
-        return None
-
-    # Lvl 1 filter
-    def filterUnion(self):
+    def filter_union(self):
         return None
 
     # Lvl 2 filter
-    def filterIntersectMostPlayed(self):
+    def filter_intersect_most_played(self):
         return None
 
     # Lvl 2 filter
-    def filterUnionMostPlayed(self):
+    def filter_union_most_played(self):
         return None
 
     # Lvl 3 filter
-    def filterUnionSimilarities(self):
+    def filter_union_similarities(self):
         return None
 
     # Lvl 4 filter
-    def filterByLength(self):
+    def filter_by_length(self):
         return None
--- app/playlistgeneration/genre_fac.py
+++ app/playlistgeneration/genre_fac.py
@@ -1,10 +1,10 @@
 # This factory generates a playlist when the song pool is narrowed
-# down to music relating to specified genres and pre-existing playlists
+# down to music relating to specified genres
 # TODO: Complete
 
 from PlaylistFactory import PlaylistFactory
 
-class UserGenreNarrowedPlaylistFactory(PlaylistFactory):
+class UserGenrePlaylistFactory(PlaylistFactory):
 
     def __init__(self):
         return None
@@ -14,25 +14,25 @@ class UserGenreNarrowedPlaylistFactory(PlaylistFactory):
         return None
 
     # Lvl 1 filter
-    def filterIntersect(self):
+    def filter_intersect(self):
         return None
 
     # Lvl 1 filter
-    def filterUnion(self):
+    def filter_union(self):
         return None
 
     # Lvl 2 filter
-    def filterIntersectMostPlayed(self):
+    def filter_intersect_most_played(self):
         return None
 
     # Lvl 2 filter
-    def filterUnionMostPlayed(self):
+    def filter_union_most_played(self):
         return None
 
     # Lvl 3 filter
-    def filterUnionSimilarities(self):
+    def filter_union_similarities(self):
         return None
 
     # Lvl 4 filter
-    def filterByLength(self):
+    def filter_by_length(self):
         return None
