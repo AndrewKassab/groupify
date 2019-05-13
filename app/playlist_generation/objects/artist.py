7cb9d20863118248b26d860ee8a96cf57d890be4
Kameron Chehraz <kchehraz@yahoo.com>
2019-05-12 22:46:17 -0700

    Changed to genres, added some commented function calls

--- app/playlist_generation/objects/artist.py
+++ app/playlist_generation/objects/artist.py
@@ -3,7 +3,7 @@ class Artist:
 
     def __init__(self, artist_id):
         self.artist_id = id
-        self.genres = None # List of genres
+        self.genres = None # List of genre ids
         self.top_songs = None # List of songids 
         __retrieve_top_songs(self)
 
@@ -14,8 +14,8 @@ class Artist:
         # add each songid to self.top_songs
         # Limit of 50 songs, can use 'short_term' 'medium_term' and 'long_term'
 
-        # top_songs_list = sp.current_user_top_tracks(limit=50, time_range='long_term') # API call: returns dict of song list info
-        # self.top_songs =[item['id'] for item in top_songs_list['items']] # Return the list of song ids
+        # top_songs_list = sp.artist_top_tracks(self.artist_id) # API call: returns dict of song list info
+        # self.top_songs = [item['id'] for item in top_songs_list['items']] # Return the list of song ids
 
     # TODO:
     def __retrieve_genres(self):
