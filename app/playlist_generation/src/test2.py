0b3540aa09f3fb43801bd9e493e3223503075bb9
unknown <Thomas.Limperis@gmail.com>
2019-06-01 16:28:23 -0700

    test3 added

--- app/playlist_generation/src/test2.py
+++ app/playlist_generation/src/test2.py
@@ -36,8 +36,11 @@ playlistid4 = "37i9dQZF1DWXXQuxfjYVxb" #this is cardi b
 username5= "groupifytest3@googlemail.com"
 username6 = "groupifytest3@gmail.com"
 playlistid6 = "37i9dQZF1DX0XUsuxWHRQd"
-username7 = "fatalis222"
-playlistid7 = "1lzlp5PJ81Wjy80nCjp0aN"
+username5 = "fatalis222"
+playlistid5 = "1lzlp5PJ81Wjy80nCjp0aN"
+
+username6 = "horsedeg"
+playlist = "5I1oCc1BG111hGbMblWPLf"
 
 
 scope = 'user-read-recently-played user-top-read user-library-modify user-library-read playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative'
@@ -45,6 +48,8 @@ token1 = util.prompt_for_user_token(username1, scope, client_id='7d2739378e2a47d
 token2 = util.prompt_for_user_token(username2, scope, client_id='7d2739378e2a47d8bc6cc89c63c5c4b0', client_secret='5d2535acb98847c5b166fadaee4fe436',redirect_uri='http://localhost:8888/callback/')
 token3 = util.prompt_for_user_token(username3, scope, client_id='7d2739378e2a47d8bc6cc89c63c5c4b0', client_secret='5d2535acb98847c5b166fadaee4fe436',redirect_uri='http://localhost:8888/callback/')
 token4 = util.prompt_for_user_token(username3, scope, client_id='7d2739378e2a47d8bc6cc89c63c5c4b0', client_secret='5d2535acb98847c5b166fadaee4fe436',redirect_uri='http://localhost:8888/callback/')
+token5 = util.prompt_for_user_token(username3, scope, client_id='7d2739378e2a47d8bc6cc89c63c5c4b0', client_secret='5d2535acb98847c5b166fadaee4fe436',redirect_uri='http://localhost:8888/callback/')
+token6 = util.prompt_for_user_token(username3, scope, client_id='7d2739378e2a47d8bc6cc89c63c5c4b0', client_secret='5d2535acb98847c5b166fadaee4fe436',redirect_uri='http://localhost:8888/callback/')
 
 
 users = {}
@@ -52,5 +57,7 @@ users[username1] = playlistid1
 users[username2] = playlistid2
 users[username3] = playlistid3
 users[username4] = playlistid4
+
+
 test = createplaylist
-test.create_playlist([username1,username4],[token1,token4],users, 500000)
+test.create_playlist([username5,username4],[token1,token4],users, 500000)
