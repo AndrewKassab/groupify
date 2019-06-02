0c57cd95cbe1f363b918f17df73c002ad8be0e0e
andrewkassab <andrewkassab@live.com>
2019-06-02 15:57:05 -0700

    moved test files

--- app/playlist_generation/test/test_with_users.py
+++ app/playlist_generation/test/test_with_users.py
@@ -1,9 +1,19 @@
 import spotipy
 import spotipy.util as util
 import os
-import sys
 
+#Added
+import createplaylist
+
+username1 = ""
+username2 = "" 
+
+token1 = ''
+token2 = ''
 scope = 'user-read-recently-played user-top-read user-library-modify user-library-read playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative'
-token1 = util.prompt_for_user_token(sys.argv[1], scope, client_id='7d2739378e2a47d8bc6cc89c63c5c4b0', client_secret='5d2535acb98847c5b166fadaee4fe436',redirect_uri='http://localhost:8888/callback/')
 
-print(token1)
+users = {}
+users[username1] = []
+users[username2] = []
+test = createplaylist
+test.create_playlist([username1,username2],[token1,token2],users, 3600000)
