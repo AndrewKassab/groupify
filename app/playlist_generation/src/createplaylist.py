e7381868da045ae75c6cc9c558d67dccef8fee02
Alexander <argibson@ucsd.edu>
2019-06-02 23:49:07 -0700

    hi

--- app/playlist_generation/src/createplaylist.py
+++ app/playlist_generation/src/createplaylist.py
@@ -7,7 +7,7 @@ def create_playlist(name, usernames, tokens, user_playlist_ids, desired_length):
 
   users = []
   for username, token in zip(usernames, tokens):
-    users.append(user(username, token, user_playlist_ids[username]))
+    users.append(user(username, token, user_playlist_ids))#username]))
 
   my_factory = PlaylistFactory(name, users, desired_length)
 
@@ -15,7 +15,7 @@ def create_playlist(name, usernames, tokens, user_playlist_ids, desired_length):
   # ths means that usernames[0] needs to have the main groupify user
   # who is creating the playlist in the firstplace
   my_factory.create(users[0])
-  
+
   return my_factory.get_track_objects()
 
   # TODO: Have each other user follow the newly created playlist
