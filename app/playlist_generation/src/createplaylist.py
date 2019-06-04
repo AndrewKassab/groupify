2019aa3c5c7344f4683b1f8cf297e75f7c24edea
andrewkassab <andrewkassab@live.com>
2019-06-04 15:27:50 -0700

    playlist tracks now only go into the pool of the user specified

--- app/playlist_generation/src/createplaylist.py
+++ app/playlist_generation/src/createplaylist.py
@@ -7,7 +7,7 @@ def create_playlist(name, usernames, tokens, user_playlist_ids, desired_length):
 
   users = []
   for username, token in zip(usernames, tokens):
-    users.append(user(username, token, user_playlist_ids))
+    users.append(user(username, token, user_playlist_ids[username]))
 
   my_factory = PlaylistFactory(name, users, desired_length)
 
