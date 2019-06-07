bfad4b73e9607219d59b4e8c928b5dd5bd7787c1
Winston Durand <me@winstondurand.com>
2019-06-07 14:42:03 -0700

    Fixed a bit of stuff

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,13 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import LoginPage from './containers/LoginPage';
 import Main from './containers/Main';
 import Callback from './containers/Callback';
+import About from './containers/About';
 import Client from './Client';
 
 const loggedIn = (
   <Switch>
     <Route component={Main} path="/playlists" />
+    <Route component={About} exact path="/about" />
     <Route render={() => <Redirect to='/playlists' />} />
   </Switch>
 );
